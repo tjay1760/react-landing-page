306b4e98392877908070eb0bff0a9496c8224224
tjay1760 <mcjthiongo@gmail.com>
2024-10-31 03:59:44 +0300

    viewing div

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-       <p className='text-3xl font-bold underline'>test sentence</p>
+       <p className='text-3xl font-bold underline text-white'>test sentence</p>
     </div>
   )
 }
