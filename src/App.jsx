b5ded89733759ea40e93142f00d27a7c8a5b4a69
tjay1760 <mcjthiongo@gmail.com>
2024-10-31 23:52:23 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,15 @@ import styles from "./styles";
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}></div>
+      <div className={`${styles.boxWidth}`}>
       Navbar
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+
     </div>
   </div>
 );
